9bd3f45fbfd9301341fe799f51a1052c4228c709
mhickman126@outlook.com <65835914+mhickman126@users.noreply.github.com>
2023-04-08 23:18:45 -0400

    Add CSS

--- src/App.js
+++ src/App.js
@@ -5,6 +5,15 @@ export default function App() {
   return (
     <div className="App">
       <Weather />
+      <footer className="footer">
+        <a
+          className="footer"
+          href="https://github.com/mhickman126/weather-react-app"
+        >
+          Open-source code
+        </a>
+        &nbsp;by Michelle Hickman
+      </footer>
     </div>
   );
 }
--- src/Weather.js
+++ src/Weather.js
@@ -1,82 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import "./Weather.css";
 
 export default function Weather() {
-  return (
-    <div className="Weather">
-      <div className="weather-app-wrapper">
-        <div className="weather-app">
-          <form id="search-form" className="mb-4">
-            <div className="row">
-              <div className="col-9">
-                <input
-                  type="search"
-                  placeholder="Please enter a city"
-                  className="form-control"
-                  id="city-input"
-                  autocomplete="off"
-                />
+  const [ready, setReady] = useState(false);
+  const [temperature, setTemperature] = useState(null);
 
-                <div className="col-3">
-                  <input
-                    type="submit"
-                    value="Search"
-                    className="btn w-100 btn-outline-light"
-                  />
-                </div>
-              </div>
+  function handleResponse(response) {
+    console.log(response);
+  }
+
+  if (ready) {
+    return (
+      <div className="weather">
+        <form>
+          <div className="row">
+            <div className="col-9">
+              <input
+                type="serach"
+                placeholder="Enter a city..."
+                class="form-control"
+              />
             </div>
-          </form>
+            <div className="col-3">
+              <input type="submit" value="Search" />
+            </div>
+          </div>
+        </form>
 
-          <div className="overview">
-            <h1 id="city">City Name</h1>
-            <ul className="date-condition">
-              <li>
-                {" "}
-                Last updated: <span id="date"></span>
-              </li>
-              <li id="description"></li>
-            </ul>
+        <h1>Atlanta</h1>
+        <ul>
+          <li>Sunday 09:00</li>
+          <li>Light Drizzle</li>
+        </ul>
+        <div className="row">
+          <div className="col-6">
+            <img src="src/weathericon.png" alt="Light Drizzle" />
+            7°C
           </div>
 
-          <div className="row">
-            <div className="col-4 left">
-              <ul>
-                <li>
-                  Humidity: <span id="humidity"></span>%
-                </li>
-                <li>
-                  Wind: <span id="wind"></span> km/h
-                </li>
-              </ul>
-            </div>
-            <div className="col-8 right">
-              <div className="clearfix weather-temperature">
-                <img src="" alt="" className="float-left" id="icon" />
-                <strong id="temperature"></strong>
-                <span className="units">
-                  <a href="/" id="celsius-link">
-                    °C
-                  </a>{" "}
-                  |{" "}
-                  <a href="/" id="fahrenheit-link">
-                    °F
-                  </a>
-                </span>
-              </div>
-            </div>
+          <div className="col-6">
+            <ul>
+              <li>Precipitation: 15%</li>
+              <li>Humidity: 88%</li>
+              <li>Wind: 26 km/h</li>
+            </ul>
           </div>
         </div>
       </div>
-      <footer className="footer">
-        <a
-          className="footer"
-          href="https://github.com/mhickman126/weather-react-app"
-        >
-          Open-source code
-        </a>
-        &nbsp;by Michelle Hickman
-      </footer>
-    </div>
-  );
+    );
+  } else {
+    const apiKey = "a54033ao500493763d370ef6f91a68tb";
+    let city = "Atlanta";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+    axios.get(apiUrl).then(handleResponse);
+  }
 }
