418bc54275da4c1b82ea8f2ec7f23346f43faba6
mhickman126@outlook.com <65835914+mhickman126@users.noreply.github.com>
2023-04-10 20:08:12 -0400

    Rearrange elements

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -6,11 +6,21 @@ import WeatherTemperature from "./WeatherTemperature";
 export default function WeatherInfo(props) {
   return (
     <div className="WeatherInfo">
-      <div className="Loc-time">
-        <h1>{props.data.city}</h1>
-        <p className="date-time">
-          <FormattedDate date={props.data.date} />
-        </p>
+      <div className="row">
+        <div className="col-7">
+          <div className="Loc-time">
+            <h1>{props.data.city}</h1>
+            <p className="date-time">
+              <FormattedDate date={props.data.date} />
+            </p>
+          </div>
+        </div>
+        <div className="col-5">
+          <ul>
+            <li>Humidity: {props.data.humidity} %</li>
+            <li>Wind: {props.data.wind} km/h</li>
+          </ul>
+        </div>
       </div>
 
       <div className="row">
@@ -26,12 +36,7 @@ export default function WeatherInfo(props) {
       </div>
       <div className="row">
         <div className="col-7"></div>
-        <div className="col-5">
-          <ul>
-            <li>Humidity: {props.data.humidity} %</li>
-            <li>Wind: {props.data.wind} km/h</li>
-          </ul>
-        </div>
+        <div className="col-5"></div>
       </div>
     </div>
   );
